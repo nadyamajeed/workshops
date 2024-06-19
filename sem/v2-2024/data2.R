cb1e8fec6103091dfb825cf859c09db80dbfed0e
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2024-06-19 17:36:24 +0800

    Create data2.R

---
+++ sem/v2-2024/data2.R
@@ -0,0 +1,44 @@
+##### START OF CODE #####
+
+# R version 4.4.0
+library(lavaan) # verion 0.6-17
+
+# generate data
+set.seed(0)
+PID = 1:500
+possibleFactorLoadings = seq(0.4, 0.9, 0.05)
+temp = rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))
+eta1 = 0.4*temp + rnorm(n = 500, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
+eta2 = 0.4*temp + rnorm(n = 500, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
+
+# organise all data
+myData = 
+  data.frame(
+    PID = PID,
+    i1 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i2 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i3 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i4 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i5 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i6 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i7 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i8 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 500, m = 0, sd = sample(seq(0.1, 1, 0.1), 1)))
+  ) |>
+  round()
+
+# test a simple cfa model
+# (check for any convergence issues)
+# (don't want a complicated example that can't run)
+lavaan::sem(
+  "
+  eta1 =~ i1 + i2 + i3 + i4
+  eta2 =~ i5 + i6 + i7 + i8
+  eta1 ~~ eta2
+  ",
+  data = myData
+) |> summary()
+
+# write data
+write.csv(myData, "data2.csv", row.names = FALSE)
+
+##### END OF CODE #####
