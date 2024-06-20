e919efe82abfdfbb01b60aa116164c475e7937df
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2024-06-20 14:56:50 +0800

    Update data3.R
    added fourth latent variable

--- sem/v2-2024/dataGeneration/data3.R
+++ sem/v2-2024/dataGeneration/data3.R
@@ -4,26 +4,30 @@
 library(lavaan) # version 0.6-17
 
 # generate data
-set.seed(2024)
-PID = 1:400
+set.seed(0)
+PID = 1:1100
 possibleFactorLoadings = seq(0.4, 0.7, 0.05)
-eta1 = rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))
-eta2 = 0.3*eta1 + rnorm(n = 400, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
-eta3 = 0.3*eta2 - 0.2*eta1 + rnorm(n = 400, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
+eta1 = rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))
+eta2 = 0.5*eta1 + rnorm(n = 1100, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
+eta3 = 0.3*eta2 - 0.2*eta1 + rnorm(n = 1100, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
+eta4 = 0.3*eta1 - 0.2*eta2 + rnorm(n = 1100, m = 0, sd = sample(seq(0.2, 1, 0.1), 1))
 
 # organise all data
 myData = 
   data.frame(
     PID = PID,
-    i1 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i2 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i3 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i4 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i5 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i6 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i7 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i8 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
-    i9 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 400, m = 0, sd = sample(seq(0.1, 1, 0.1), 1)))
+    i01 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i02 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i03 = 3*(sample(possibleFactorLoadings, 1)*eta1 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i04 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i05 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i06 = 3*(sample(possibleFactorLoadings, 1)*eta2 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i07 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i08 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i09 = 3*(sample(possibleFactorLoadings, 1)*eta3 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i10 = 3*(sample(possibleFactorLoadings, 1)*eta4 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i11 = 3*(sample(possibleFactorLoadings, 1)*eta4 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1))),
+    i12 = 3*(sample(possibleFactorLoadings, 1)*eta4 + rnorm(n = 1100, m = 0, sd = sample(seq(0.1, 1, 0.1), 1)))
   ) |>
   round()
 
@@ -32,11 +36,13 @@ myData =
 # (don't want a complicated example that can't run)
 lavaan::sem(
   "
-  eta1 =~ i1 + i2 + i3 
-  eta2 =~ i4 + i5 + i6
-  eta3 =~ i7 + i8 + i9
-  eta3 ~ eta2 + eta1
+  eta1 =~ i01 + i02 + i03 
+  eta2 =~ i04 + i05 + i06
+  eta3 =~ i07 + i08 + i09
+  eta4 =~ i10 + i11 + i12
+  eta4 + eta3 ~ eta2 + eta1
   eta2 ~ eta1
+  eta4 ~~ eta3
   ",
   data = myData
 ) |> summary(fit.measures = TRUE)
