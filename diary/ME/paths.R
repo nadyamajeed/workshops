44e3af5890ce89f16f5517fc1ad2080553826b53
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2024-10-17 16:58:24 +0800

    Update paths.R

--- diary/ME/paths.R
+++ diary/ME/paths.R
@@ -1,13 +1,12 @@
 ##### START OF CODE #####
-# version 1.0 (240814)
+# version 2.0 (241017)
 
-# R version 4.4.0
+# R version 4.4.1
 library(dplyr)       # version 1.1.4
 library(tidyr)       # version 1.3.1
-library(lme4)        # version 1.1-35.4
-library(lavaan)      # version 0.6-17
-library(effectsize)  # version 0.8.9
-library(broom.mixed) # version 0.2.9.5
+library(lme4)        # version 1.1-35.5
+library(lavaan)      # version 0.6-19
+library(broom.mixed) # version 0.2.9.6
 
 ##### CUSTOM HELPER FUNCTIONS #####
 
@@ -51,26 +50,6 @@ appr1_nomis = lme4::lmer(
       LON = rowMeans(across(starts_with("LON"))),
       ANX = rowMeans(across(starts_with("ANX"))),
       RUM = rowMeans(across(starts_with("RUM")))
-    ) %>%
-    dplyr::group_by(PID) %>%
-    dplyr::mutate(
-      ANXb = mean(ANX),
-      ANXw = ANX - ANXb,
-      RUMb = mean(RUM),
-      RUMw = RUM - RUMb
-    ) %>%
-    dplyr::ungroup()
-) %>% mlm_unstd_and_std()
-
-# RUM misspecified?
-
-appr1_mis = lme4::lmer(
-  LON ~ 1 + ANXb + ANXw + RUMb + RUMw + (1 | PID),
-  data = dataFull %>%
-    dplyr::mutate(
-      LON = rowMeans(across(starts_with("LON"))),
-      ANX = rowMeans(across(starts_with("ANX"))),
-      RUM = rowMeans(across(c(RUM1, ENV1, RUM3)))
     ) %>%
     dplyr::group_by(PID) %>%
     dplyr::mutate(
@@ -139,35 +118,6 @@ appr2_nomis = lme4::lmer(
     dplyr::ungroup()
 ) %>% mlm_unstd_and_std()
 
-# RUM misspecified?
-
-temp3 = lme4::lmer(
-  value ~ 1 + (1 | PID/DAY),
-  data = dataFull %>%
-    dplyr::select(PID, DAY, RUM1, ENV1, RUM3) %>%
-    tidyr::pivot_longer(c(RUM1, ENV1, RUM3)) %>%
-    dplyr::rename(item = name)
-) %>% coef(); temp3 = temp3$`DAY:PID`; temp3 = temp3 %>%
-  dplyr::mutate(
-    temp = rownames(.),
-    PID = substr(temp, nchar(temp) - 3, nchar(temp)),
-    DAY = substr(temp, 1, 1),
-    temp = NULL) %>%
-  dplyr::rename(RUM = `(Intercept)`)
-
-appr2_mis = lme4::lmer(
-  LON ~ 1 + ANXb + ANXw + RUMb + RUMw + (1 | PID),
-  data = merge(merge(temp1, temp2), temp3) %>%
-    dplyr::group_by(PID) %>%
-    dplyr::mutate(
-      ANXb = mean(ANX),
-      ANXw = ANX - ANXb,
-      RUMb = mean(RUM),
-      RUMw = RUM - RUMb
-    ) %>%
-    dplyr::ungroup()
-) %>% broom.mixed::tidy()
-
 ##### SAM-L #####
 
 appr3_nomis = lavaan::sam(
@@ -181,27 +131,6 @@ appr3_nomis = lavaan::sam(
   LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
   ANX =~ ANX1 + ANX2 + ANX3
   RUM =~ RUM1 + RUM2 + RUM3
-  LON ~ ANX + RUM
-  ",
-  mm.list = list(LON = "LON", ANX = "ANX", RUM = "RUM"),
-  sam.method = "local",
-  cluster = "PID",
-  data = dataFull
-) %>% broom.mixed::tidy()
-
-# RUM misspecified?
-
-appr3_mis = lavaan::sam(
-  "
-  level: 1
-  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
-  ANX =~ ANX1 + ANX2 + ANX3
-  RUM =~ RUM1 + ENV1 + RUM3
-  LON ~ ANX + RUM
-  level: 2
-  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
-  ANX =~ ANX1 + ANX2 + ANX3
-  RUM =~ RUM1 + ENV1 + RUM3
   LON ~ ANX + RUM
   ",
   mm.list = list(LON = "LON", ANX = "ANX", RUM = "RUM"),
@@ -231,6 +160,99 @@ appr4_nomis = lavaan::sam(
   data = dataFull
 ) %>% broom.mixed::tidy()
 
+##### FULL SEM #####
+
+appr5_nomis = lavaan::sem(
+  "
+  level: 1
+  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
+  ANX =~ ANX1 + ANX2 + ANX3
+  RUM =~ RUM1 + RUM2 + RUM3
+  LON ~ ANX + RUM
+  level: 2
+  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
+  ANX =~ ANX1 + ANX2 + ANX3
+  RUM =~ RUM1 + RUM2 + RUM3
+  LON ~ ANX + RUM
+  ",
+  cluster = "PID",
+  data = dataFull
+) %>% broom.mixed::tidy()
+
+##### END OF CODE #####
+
+##### DUMP FOR MISSPECIFICATION #####
+
+# RUM misspecified?
+
+appr1_mis = lme4::lmer(
+  LON ~ 1 + ANXb + ANXw + RUMb + RUMw + (1 | PID),
+  data = dataFull %>%
+    dplyr::mutate(
+      LON = rowMeans(across(starts_with("LON"))),
+      ANX = rowMeans(across(starts_with("ANX"))),
+      RUM = rowMeans(across(c(RUM1, ENV1, RUM3)))
+    ) %>%
+    dplyr::group_by(PID) %>%
+    dplyr::mutate(
+      ANXb = mean(ANX),
+      ANXw = ANX - ANXb,
+      RUMb = mean(RUM),
+      RUMw = RUM - RUMb
+    ) %>%
+    dplyr::ungroup()
+) %>% mlm_unstd_and_std()
+
+# RUM misspecified?
+
+temp3 = lme4::lmer(
+  value ~ 1 + (1 | PID/DAY),
+  data = dataFull %>%
+    dplyr::select(PID, DAY, RUM1, ENV1, RUM3) %>%
+    tidyr::pivot_longer(c(RUM1, ENV1, RUM3)) %>%
+    dplyr::rename(item = name)
+) %>% coef(); temp3 = temp3$`DAY:PID`; temp3 = temp3 %>%
+  dplyr::mutate(
+    temp = rownames(.),
+    PID = substr(temp, nchar(temp) - 3, nchar(temp)),
+    DAY = substr(temp, 1, 1),
+    temp = NULL) %>%
+  dplyr::rename(RUM = `(Intercept)`)
+
+appr2_mis = lme4::lmer(
+  LON ~ 1 + ANXb + ANXw + RUMb + RUMw + (1 | PID),
+  data = merge(merge(temp1, temp2), temp3) %>%
+    dplyr::group_by(PID) %>%
+    dplyr::mutate(
+      ANXb = mean(ANX),
+      ANXw = ANX - ANXb,
+      RUMb = mean(RUM),
+      RUMw = RUM - RUMb
+    ) %>%
+    dplyr::ungroup()
+) %>% broom.mixed::tidy()
+
+# RUM misspecified?
+
+appr3_mis = lavaan::sam(
+  "
+  level: 1
+  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
+  ANX =~ ANX1 + ANX2 + ANX3
+  RUM =~ RUM1 + ENV1 + RUM3
+  LON ~ ANX + RUM
+  level: 2
+  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
+  ANX =~ ANX1 + ANX2 + ANX3
+  RUM =~ RUM1 + ENV1 + RUM3
+  LON ~ ANX + RUM
+  ",
+  mm.list = list(LON = "LON", ANX = "ANX", RUM = "RUM"),
+  sam.method = "local",
+  cluster = "PID",
+  data = dataFull
+) %>% broom.mixed::tidy()
+
 # RUM misspecified?
 
 appr4_mis = lavaan::sam(
@@ -248,25 +270,6 @@ appr4_mis = lavaan::sam(
   ",
   mm.list = list(LON = "LON", ANX = "ANX", RUM = "RUM"),
   sam.method = "global",
-  cluster = "PID",
-  data = dataFull
-) %>% broom.mixed::tidy()
-
-##### FULL SEM #####
-
-appr5_nomis = lavaan::sem(
-  "
-  level: 1
-  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
-  ANX =~ ANX1 + ANX2 + ANX3
-  RUM =~ RUM1 + RUM2 + RUM3
-  LON ~ ANX + RUM
-  level: 2
-  LON =~ LON1 + LON2 + LON3 + LON4 + LON5 + LON6
-  ANX =~ ANX1 + ANX2 + ANX3
-  RUM =~ RUM1 + RUM2 + RUM3
-  LON ~ ANX + RUM
-  ",
   cluster = "PID",
   data = dataFull
 ) %>% broom.mixed::tidy()
@@ -290,4 +293,4 @@ appr5_mis = lavaan::sem(
   data = dataFull
 ) %>% broom.mixed::tidy()
 
-##### END OF CODE #####
+##### END OF DUMP #####
