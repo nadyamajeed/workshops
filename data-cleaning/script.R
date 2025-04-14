aa968268673ed2fc865874e87742d15d3f82eab9
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2025-04-14 13:44:53 +0800

    Update script.R
    updated versions + made it clearer which functions are from which packages + added code to extract just the alpha & 95% ci (feldt) for inter item consistency / reliability part

--- data-cleaning/script.R
+++ data-cleaning/script.R
@@ -1,58 +1,75 @@
+##### START OF CODE #####
+
+# R version 4.4.1
+
 # load libraries
 # indicate versions for reproducibility
-library(dplyr) # version 1.0.7
-library(haven) # version 2.4.3
-library(psych) # version 2.1.6
+library(dplyr) # version 1.1.4
+library(haven) # version 2.5.4
+library(psych) # version 2.5.3
 
 ##### import and inspect data #####
 
 # read in data
-data.raw = read_sav("https://github.com/nadyamajeed/workshops/raw/main/data-cleaning/data-cleaning.sav")
+data.raw = haven::read_sav("https://raw.githubusercontent.com/nadyamajeed/workshops/refs/heads/main/data-cleaning/data-cleaning.sav")
 
 # briefly inspect the data
-data.raw %>% glimpse()
-data.raw %>% describe()
+data.raw %>% dplyr::glimpse()
+data.raw %>% psych::describe()
 
 ##### basic cleaning of invalid data #####
 
 data.fix = data.raw %>%
-  mutate(
+  dplyr::mutate(
     # remove negative values in teaparty_per_week
     teaparty_per_week = ifelse(teaparty_per_week < 0, NA_real_, teaparty_per_week),
     # remove values > 4.3 in gpa
     gpa = ifelse(gpa > 4.3, NA_real_, gpa)
   )
 
+# briefly inspect the data
+data.fix %>% dplyr::glimpse()
+data.fix %>% psych::describe()
+
 ##### score psychometric scales and get reliability #####
 
 # score the agreeableness measure
 data.scored = data.fix %>%
-  mutate(
+  dplyr::mutate(
     # reverse code agr2, agr4, agr6
-    agr2 = 6 - agr2,
-    agr4 = 6 - agr4,
-    agr6 = 6 - agr6,
+    dplyr::across(
+      .cols = c(agr2, agr4, agr6),
+      .fns = ~ 6 - .x
+    ),
     # aggregate
     agree_avg = rowMeans(across(agr1:agr6))
   )
 
 # get cronbach's alpha for agreeableness
-data.scored %>%
-  select(agr1:agr6) %>%
-  alpha()
+agr.alpha = data.scored %>%
+  dplyr::select(agr1:agr6) %>%
+  psych::alpha()
+
+# cronbach's alpha - full output
+print(agr.alpha)
+
+# cronbach's alpha - just alpha & 95% CI (feldt)
+print(agr.alpha$feldt)
 
 ##### get descriptives from final data #####
 
 # for everything
 data.scored %>%
-  describe()
+  psych::describe()
 
 # for only some things we care about
 data.scored %>%
-  select(teaparty_per_week, gpa, agree_avg) %>%
-  describe()
+  dplyr::select(teaparty_per_week, gpa, agree_avg) %>%
+  psych::describe()
 
 ##### write our final data (in two formats) for future use #####
 
 write.csv(data.scored, file = "final.csv", row.names = FALSE)
 write_sav(data.scored, file = "final.sav")
+
+##### END OF CODE #####
