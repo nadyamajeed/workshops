4bbb25d5da4420354a80358fef3315664509eaca
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2025-07-18 05:05:48 +0800

    Update dataSimPasta.R

--- regression/2025/data/dataSimPasta.R
+++ regression/2025/data/dataSimPasta.R
@@ -1,21 +1,21 @@
 library(dplyr)
 
 data.frame(
-  id = 1:500
+  id = 1:5000
 ) %>%
   dplyr::mutate(
     pastasPerDay = sample(
-      seq(0, 6, 1),
+      seq(0, 6, 0.5),
       size = nrow(.),
       replace = TRUE),
     pastaLiking = sample(
-      seq(-3, 3, 0.01),
+      seq(-10, 10, 0.01),
       size = nrow(.),
       replace = TRUE),
     happinessScore = 
       rnorm(nrow(.)) + 
-      0.3*pastasPerDay +
-      0.1*pastaLiking +
-      0.5*pastasPerDay*pastaLiking
+      0.01*pastasPerDay +
+      0.001*pastaLiking +
+      0.008*pastasPerDay*pastaLiking
   ) %>%
   write.csv("dataSimPasta.csv", row.names = FALSE)
